419152b244b96bed6ba212551527a81c389b55e8
Elliot Stokes <elliot.stokes@gmail.com>
2014-11-10 17:05:04 +0000

    update

--- assets/themes/twitter/js/pop-page.js
+++ assets/themes/twitter/js/pop-page.js
@@ -1,15 +1,17 @@
 var jumboHeight = $('.jumbotron').outerHeight();
+var lead = $(".jumbotron div.sub");
+var header = $(".jumbotron h1");
+
 function parallax() {
     var scrolled = $(window).scrollTop();
     if (scrolled <0) {
 		scrolled = 0;
     }
-    var lead = $(".jumbotron div.sub");
-    var header = $(".jumbotron h1");
 
     if (scrolled < jumboHeight) {
 		var percentScrolled = 1- (scrolled * 1.9)/jumboHeight;
 		header.css('opacity', percentScrolled);
+        header.css("transform", 'translate(' + scrolled + 'px,0' + ")");
 		lead.css("transform", 'translate(0,' + scrolled + 'px' + ")");
     }
 }
